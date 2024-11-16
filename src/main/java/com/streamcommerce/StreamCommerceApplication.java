7d64e124988e281794dd1a0e175015cba1d3a3e5
camperfire <aliemirhanabanozoglu@gmail.com>
2024-11-16 16:01:23 +0300

    Initialize Project with Spring Initializr

---
+++ src/main/java/com/streamcommerce/StreamCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.streamcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streamcommerce/StreamCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
